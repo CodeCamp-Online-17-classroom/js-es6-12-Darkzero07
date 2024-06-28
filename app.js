a51bb777f674267aa3b1ef2b8ba3e3f886bcf1b8
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-29 03:40:20 +0700

    js lab 156

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+// ผลลัพธ์ในบรรทัดที่มี * มีค่าเป็นอะไรและเพราะอะไร
+
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+  'Raindrops on roses',
+  'whiskers on kittens',
+  'Bright copper kettles',
+  'warm woolen mittens',
+];
+console.log(raindrops); // Raindrops on roses
+//ป็นการใช้ array destructuring ซึ่งอ้างอิงค่าตาม index ใน array
+console.log(whiskers); //whiskers on kittens
+//ป็นการใช้ array destructuring ซึ่งอ้างอิงค่าตาม index ใน array
+console.log(aFewOfMyFavoriteThings); // [ 'Bright copper kettles', 'warm woolen mittens' ]
+//ป็นการใช้  spread operator กับ array destructuring ซึ่งอ้างอิงค่าตาม index ใน array ที่เหลือทั้งหมด
